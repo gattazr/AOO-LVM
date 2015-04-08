00dffd8e04fb7a7688015a1166d548e74fbb76f3
gattazr <remi.gattaz@gmail.com>
2015-04-08 11:13:53 +0200

    add Context

---
+++ AOO-LVM/src/ricm/aoo/lvm/Context.java
@@ -0,0 +1,84 @@
+package ricm.aoo.lvm;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import ricm.aoo.lvm.kernel.SExpr;
+import ricm.aoo.lvm.kernel.Symbol;
+
+public class Context {
+
+	public static Context CONTEXT = new Context();
+
+	private List<Map<Symbol, SExpr>> pContext;
+
+	private Context() {
+		this.pContext = new LinkedList<Map<Symbol, SExpr>>();
+		this.pContext.add(new HashMap<Symbol, SExpr>());
+	}
+
+	/**
+	 * TODO: comments
+	 *
+	 * @param aSymbole
+	 * @param aExpr
+	 */
+	public void addToContext(Symbol aSymbole, SExpr aExpr) {
+		this.pContext.get(this.pContext.size() - 1).put(aSymbole, aExpr);
+	}
+
+	/**
+	 * TODO: comments
+	 *
+	 * @param aSymbole
+	 * @param aExpr
+	 */
+	public void addToContextGeneral(Symbol aSymbole, SExpr aExpr) {
+		this.pContext.get(1).put(aSymbole, aExpr);
+	}
+
+	/**
+	 * Retire le dernier contexte
+	 */
+	public void popContext() {
+		this.pContext.remove(this.pContext.size() - 1);
+	}
+
+	/**
+	 * Rajoute un contexte
+	 */
+	public void pushContext() {
+		this.pContext.add(new HashMap<Symbol, SExpr>());
+	}
+
+	/**
+	 * recherche la valeur associé au symbole passé en paramètre dans les
+	 * contextes. On recherche toujours dans le contexte le plus récent avant de
+	 * remonter vers les contextes les plus anciens
+	 *
+	 * @param aSymbol
+	 *            Symbole à rechercher dans les contextes
+	 * @return la SExpr lié à ce symbole dans le contexte. null si le symbole
+	 *         n'est pas dans le contexte
+	 *
+	 */
+	public SExpr search(Symbol aSymbol) {
+		SExpr wSExpr;
+		ListIterator<Map<Symbol, SExpr>> wIterator = this.pContext
+				.listIterator(this.pContext.size() - 1);
+
+		/* Itere dans chaque contexte en partant du plus récent */
+		while (wIterator.hasPrevious()) {
+			Map<Symbol, SExpr> wMap = wIterator.previous();
+			wSExpr = wMap.get(aSymbol);
+			if (wSExpr != null) {
+				return wSExpr;
+			}
+		}
+
+		return null;
+	}
+}
