b3ae47bfdfe7de7d907ca59f200b7e047d9df1fd
gattazr <remi.gattaz@gmail.com>
2015-05-10 15:26:16 +0200

    Correction de la primitive Print

--- AOO-LVM/src/ricm/aoo/lvm/primit/Print.java
+++ AOO-LVM/src/ricm/aoo/lvm/primit/Print.java
@@ -3,6 +3,7 @@ package ricm.aoo.lvm.primit;
 import ricm.aoo.lvm.Console;
 import ricm.aoo.lvm.LVMException;
 import ricm.aoo.lvm.MachineLISP;
+import ricm.aoo.lvm.kernel.Nil;
 import ricm.aoo.lvm.kernel.SExpr;
 import ricm.aoo.lvm.kernel.Subr;
 
@@ -12,12 +13,15 @@ public class Print extends Subr {
 	public SExpr execPrimitive(MachineLISP aMachineLisp, SExpr aSExpr)
 			throws LVMException {
 		int wSize = aSExpr.size();
-		SExpr wCourant = aSExpr;
-		for (int wI = 0; wI < wSize - 1; wI++) {
+		SExpr wCourant = Nil.NIL;
+		SExpr wNext = aSExpr;
+		for (int wI = 0; wI < wSize; wI++) {
+			wCourant = wNext;
 			Console.print(wCourant.car().toString());
-			wCourant = wCourant.cdr();
+			wNext = wCourant.cdr();
 		}
-		return wCourant;
+		Console.println();
+		return wCourant.car();
 	}
 
 	@Override
